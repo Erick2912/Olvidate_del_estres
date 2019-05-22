15d5a1192d32bb2869add6591208d9f12fa8261a
Erick2912 <47580112+Erick2912@users.noreply.github.com>
2019-05-21 20:47:25 -0500

    ayuda
    Ing solo tengo una parte del index el blog mas o menos completo, me faltan los demás secciones

---
+++ js/narvar.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+var prevScrollpos = window.pageYOffsetset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+});
